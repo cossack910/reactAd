3244544eaee4bd5c4de64327ee4153753fe68825
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-08-25 22:47:50 +0900

    axios

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
-import Router from "./router/Router";
+import axios from "axios";
 
 function App() {
+  const onClickComments = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
+
+  const onClickComment1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments/4")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
+
   return (
     <div className="App">
-      <Router />
+      <button onClick={onClickComments}>comments</button>
+      <button onClick={onClickComment1}>id=1:comments</button>
     </div>
   );
 }
